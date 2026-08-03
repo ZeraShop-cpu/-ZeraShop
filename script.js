8bc6ddd49323fee698e8b4cdfe98b8c4901acc26
ZeraShop-cpu <kk2826267@gmail.com>
2026-08-03 15:38:55 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,27 +1,41 @@
+// ZeraShop - Clothing Store
+
 const products = [
   {
     id: 1,
-    name: "iPhone 16 Pro",
-    price: 999,
-    image: "https://picsum.photos/300?random=1"
+    name: "Men's T-Shirt",
+    price: 25,
+    image: "https://images.unsplash.com/photo-1521572163474-6864f9cf17ab?w=600"
   },
   {
     id: 2,
-    name: "MacBook Pro",
-    price: 1999,
-    image: "https://picsum.photos/300?random=2"
+    name: "Women's Dress",
+    price: 55,
+    image: "https://images.unsplash.com/photo-1515372039744-b8f02a3ae446?w=600"
   },
   {
     id: 3,
-    name: "AirPods Pro",
-    price: 249,
-    image: "https://picsum.photos/300?random=3"
+    name: "Black Hoodie",
+    price: 40,
+    image: "https://images.unsplash.com/photo-1556821840-3a63f95609a7?w=600"
   },
   {
     id: 4,
-    name: "Apple Watch",
-    price: 399,
-    image: "https://picsum.photos/300?random=4"
+    name: "Blue Jeans",
+    price: 45,
+    image: "https://images.unsplash.com/photo-1542272604-787c3835535d?w=600"
+  },
+  {
+    id: 5,
+    name: "Winter Jacket",
+    price: 80,
+    image: "https://images.unsplash.com/photo-1541099649105-f69ad21f3246?w=600"
+  },
+  {
+    id: 6,
+    name: "White Sneakers",
+    price: 70,
+    image: "https://images.unsplash.com/photo-1542291026-7eec264c27ff?w=600"
   }
 ];
 
