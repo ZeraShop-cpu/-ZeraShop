980fc532da29279e753c10306848a6478d5c78ae
ZeraShop-cpu <kk2826267@gmail.com>
2026-08-04 05:32:42 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,30 +1,12 @@
-document.addEventListener("DOMContentLoaded", () => {
-
-    // Buy Now Buttons
-    const buttons = document.querySelectorAll(".card button");
-
-    buttons.forEach(button => {
-        button.addEventListener("click", () => {
-            alert("Product added to cart!");
-        });
-    });
-
-    // Search Icon
-    const search = document.querySelector(".fa-magnifying-glass");
-
-    if (search) {
-        search.addEventListener("click", () => {
-            alert("Search feature coming soon.");
-        });
-    }
-
-    // Hero Button
-    const heroBtn = document.querySelector(".hero button");
-
-    if (heroBtn) {
-        heroBtn.addEventListener("click", () => {
-            window.location.href = "products.html";
-        });
-    }
-
-});
+ZeraShop/
+│── index.html
+│── style.css
+│── script.js
+│── login.html
+│── products.html
+│── about.html
+│── contact.html
+│── cart.html
+│
+├── images/
+└── icons/
