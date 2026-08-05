84cbf89fb11437d71cf5a603c348dca02f71a37c
ZeraShop-cpu <kk2826267@gmail.com>
2026-08-05 06:14:51 +0300

    Update script.js

--- script.js
+++ script.js
@@ -11,18 +11,6 @@ buttons.forEach(button => {
         alert("Product added to cart ✅");
     });
 });
-const cartCount = document.getElementById("cart-count");
-const buttons = document.querySelectorAll(".card button");
-
-let count = 0;
-
-buttons.forEach(button => {
-    button.addEventListener("click", () => {
-        count++;
-        cartCount.textContent = count;
-        alert("Product added to cart ✅");
-    });
-});
 const menuToggle = document.querySelector(".menu-toggle");
 const menu = document.querySelector(".menu");
 
